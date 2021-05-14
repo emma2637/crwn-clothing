d32523bb0dff6cd59f87a23a7aa2574fd3644bdf
emma2637 <emma2637@hotmail.com>
2021-05-14 14:48:22 -0700

    update our menu items to navigate withrouter  HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
 import  HomePage from "./pages/homepage/homepage.component";
-
+import {Route, Switch} from 'react-router-dom'
 function App() {
+
+const HatsPage = ()=>(
+ <div>
+   <h1>Hats page </h1>
+ </div>
+);
+
+
+
   return (
     <div >
-     <HomePage></HomePage>
+      <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/hats' component={HatsPage}/>
+
+      
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import{withRouter} from 'react-router-dom' //takes any component and modifies it and return a modify component
+
 import './menu-item.styles.scss'
 //functional component
-const MenuItem =({title, imageUrl, id, size}) =>(
+const MenuItem =({title, imageUrl, size, history, linkUrl,match}) =>(
 //javasscript template string ``
-    <div className={`${size} menu-item`}>
+//this is equivalent to /somematchedURL/linkURL
+    <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}> 
         <div className='background-image' style={{
         backgroundImage:`url(${imageUrl})`}}/>
         
@@ -14,4 +17,4 @@ const MenuItem =({title, imageUrl, id, size}) =>(
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem); //returns back super power menu item component with access to history props
